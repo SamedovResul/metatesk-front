d501f45fed43243929ad9fe79d3811b1c4cec1b7
SamedovRasul <samedovrasul7@gmail.com>
2022-10-18 12:52:27 +0400

    fix video size

--- src/metatesk/footer.jsx
+++ src/metatesk/footer.jsx
@@ -1,44 +1,20 @@
 import React from 'react'
+import metatesk from './metaImg/metatesk-footer-logo.png'
+
 
 const Footer = () => {
+
+  const date = new Date()
+  const year = date.getFullYear()
   return (
     <footer className="footer-section">
     <div className="container">
       <div className="container-fluid" >
         <div className="row">
           <div className="col-md-12">
-            <p>© All Rights Reserved</p>
+            <p>© {year} Entesk All Rights Reserved</p>
+            <img src={metatesk} alt="" />
           </div>
-          {/* <div className="col-md-6 col-sm-6 col-6">
-            <div className="ul-box">
-              <p>Pages</p>
-              <ul>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Education</a> </li>
-                <li> <a href="">About</a></li>
-                <li> <a href="">Contact</a> </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-6 col-sm-6 col-6">
-            <div className="ul-box">
-              <p>Education</p>
-              <ul>
-                <li> <a href="">Science</a> </li>
-                <li> <a href="">Programming</a> </li>
-                <li> <a href="">Digital Art</a> </li>
-                <li> <a href="">Robotics</a> </li>
-              </ul>
-            </div>
-          </div> */}
-          {/* <div className="col-md-4">
-            <div className="ul-box">
-              <ul>
-                <li>all right reserved</li>
-                
-              </ul>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
