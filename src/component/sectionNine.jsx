189c5bb114e579f8ba75d89f8570c2341b5cd570
SamedovRasul <samedovrasul7@gmail.com>
2022-11-08 11:51:18 +0400

    comment phone date

--- src/component/sectionNine.jsx
+++ src/component/sectionNine.jsx
@@ -7,24 +7,28 @@ import countryList from 'react-select-country-list'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import { validate } from 'react-email-validator';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
-const Addsection3 = ({setboolean}) => {
+const SectionNine = ({setboolean}) => {
   const [data, setData] = useState({
     nameSurname:'',
     Country:'',
     email:'',
-    date:''
+    date:'',
+    time:'',
+    whatsAppNumber:""
   })
   const [Disabled, setDisabled] = useState(false)
-  const [Date, setDate] = useState(false)
+  const [date, setDate] = useState(false)
   const submit = (e) =>{
     e.preventDefault()
 
     if(data.Country && data.email && data.nameSurname && data.date ){
       if(validate(data.email) ){
         setDisabled(true)
-        // https://metatesk.herokuapp.com/post
-        axios.post('https://metatesk.herokuapp.com/email/post', data)
+        // https://metatesk.herokuapp.com/email/post  http://localhost:5000/email/post
+        axios.post('http://localhost:5000/email/post', data)
         .then(function (response) {
           Swal.fire({
             color:"green",
@@ -34,7 +38,10 @@ const Addsection3 = ({setboolean}) => {
           setData({
             nameSurname:'',
             Country:'',
-            email:''
+            email:'',
+            whatsAppNumber:"",
+            date:'',
+            time:'',
           })
         })
         .catch(function (error) {
@@ -55,6 +62,7 @@ const Addsection3 = ({setboolean}) => {
   }
   console.log(data)
  
+  const D = new Date()
 
   const options = useMemo(() => countryList().getData(), [])
   return (
@@ -84,15 +92,41 @@ const Addsection3 = ({setboolean}) => {
                         ...data, email: e.target.value
                       })}
                     />
-                    {/* <input type="datetime-local" value="dds" onChange={(e) => setData({
-                        ...data, date: e.target.value
-                      })}  /> */}
-                        <input placeholder="Select date" 
-                        type={Date ? "datetime-local" : "text"} 
-                        onFocus={() => setDate(true)} 
+
+                    {/* <div className='WhatsApp-div' >
+                      <span className='WhatsApp'>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
+                      </span>
+                      <PhoneInput
+                      country={"us"}
+                      type="number"  value={data.whatsAppNumber}  
+                      onChange={phone => setData({
+                        ...data, whatsAppNumber: phone
+                      })}
+                      />
+                    </div> */}
+                    {/* <div className="date-div">
+                      <input
+                        placeholder='select date' 
+                        type={date ? "date" : "text"}
+                        onFocus={() => setDate(true)}
+                        min={`${D.getFullYear()}-${D.getMonth() + 1}-0${D.getDate() + 1}`}
                         onChange={(e) => setData({
                           ...data, date: e.target.value
-                        })} ></input>
+                        })}
+                         />
+                         <input  
+                          placeholder='select time' 
+                          type={date ? "time" : "text"}
+                          onFocus={() => setDate(true)} 
+                          onChange={(e) => setData({
+                            ...data, time: e.target.value
+                          })}
+                          />
+                    </div> */}
+                      
+                       
+                        
                       {
                         Disabled ? (
                           <button
@@ -120,4 +154,4 @@ const Addsection3 = ({setboolean}) => {
   )
 }
 
-export default Addsection3
+export default SectionNine
