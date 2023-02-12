2800f47be127e7b1278f9364558a6a6211a673b5
Resul <68106627+SamedovResul@users.noreply.github.com>
2023-02-12 12:27:33 +0400

    js

--- src/metatesk/sectionOne.jsx
+++ src/metatesk/sectionOne.jsx
@@ -1,41 +1,61 @@
-import React,{useState,useRef} from 'react'
-import Slider from "react-slick";
-import img1 from './metaImg/banner.png';
-import img2 from './metaImg/banner2.png';
-import img3 from './metaImg/banner3.png';
+import React,{useState,useRef,useEffect} from 'react'
 import {Element} from 'react-scroll';
 import ScrollTriger from 'react-scroll-trigger';
+import { animated, useSpring } from '@react-spring/web'
+
 
 const SectionOne = ({setboolean}) => {
   
-const imgs = [
-  {
-    img:'https://res.cloudinary.com/dbgyytugh/image/upload/v1666609831/banner_jwv0es.png'
-  },
-  {
-    img:'https://res.cloudinary.com/dbgyytugh/image/upload/v1666609837/banner2_nvnsnl.png'
-  },
-  {
-    img:'https://res.cloudinary.com/dbgyytugh/image/upload/v1666609834/banner3_wg1k1o.png'
-  },
-]
+
 // use spring 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  speed: 3000,
-  autoplaySpeed: 4000,
-  cssEase: "linear",
-  fade: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
-};
+
+
+const [angle, setAngle] = useState(0);
+
+const [angle1, setAngle1] = useState(0);
+const [angle2, setAngle2] = useState(0);
+const [angle3, setAngle3] = useState(0);
+
+useEffect(() => {
+  const intervalId1 = setInterval(() => {
+    setAngle1(prevAngle => prevAngle + 0.05);
+  }, 20);
+
+  const intervalId2 = setInterval(() => {
+    setAngle2(prevAngle => prevAngle + 0.04);
+  }, 20);
+
+  const intervalId3 = setInterval(() => {
+    setAngle3(prevAngle => prevAngle + 0.03);
+  }, 20);
+
+  return () => {
+    clearInterval(intervalId1);
+    clearInterval(intervalId2);
+    clearInterval(intervalId3);
+  };
+  }, []);
+
+  const x1 = 20 * Math.cos(angle1);
+  const y1 = 10 * Math.sin(angle1);
+
+  const x2 = 20 * Math.cos(angle2);
+  const y2 = 10 * Math.sin(angle2);
+
+  const x3 = 20 * Math.cos(angle3);
+  const y3 = 10 * Math.sin(angle3);
+
+  const animation1 = useSpring({
+    transform: `translate(${x1}px, ${y1}px)`
+  });
+
+  const animation2 = useSpring({
+    transform: `translate(${x2}px, ${y2}px)`
+  });
+
+  const animation3 = useSpring({
+    transform: `translate(${x3}px, ${y3}px)`
+});
 
   return (
     <Element name="home" >
@@ -53,10 +73,10 @@ const settings = {
           </div>
         </div>
         <div className="img-container">
-          <img src="https://res.cloudinary.com/djzudd6ub/image/upload/v1676112842/metatesk%20web/idris_for_metatesk_site_psd_rbaoea.png" alt="metatesk" />
-          <img src="https://www.enteskedu.com/static/media/leaf.712bf782.png" alt="" />
-          <img src="https://www.enteskedu.com/static/media/circle.14705bd1.png" alt="" />
-          <img src="https://www.enteskedu.com/static/media/brush.3fc0463e.png" alt="" />
+          <img  src="https://res.cloudinary.com/djzudd6ub/image/upload/v1676112842/metatesk%20web/idris_for_metatesk_site_psd_rbaoea.png" alt="metatesk" />
+          <animated.img style={animation1} src="https://www.enteskedu.com/static/media/leaf.712bf782.png" alt="" />
+          <animated.img style={animation2} src="https://www.enteskedu.com/static/media/circle.14705bd1.png" alt="" />
+          <animated.img style={animation3} src="https://www.enteskedu.com/static/media/brush.3fc0463e.png" alt="" />
         </div>
       </article>
       </ScrollTriger>
