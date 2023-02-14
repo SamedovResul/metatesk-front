de13c7288fd9039de0d4b214860fb141e15f62c3
SamedovRasul <samedovrasul7@gmail.com>
2023-02-14 14:52:27 +0400

    section two

--- src/component/sectionTwo.jsx
+++ src/component/sectionTwo.jsx
@@ -1,102 +1,57 @@
-import React, { useState, useEffect, useRef   } from "react";
+import React, {useState,useEffect} from 'react'
 import VideoPlayer from "react-video-js-player";
-import ScrollTriger from "react-scroll-trigger";
-import img1 from "./metaImg/book.png";
-import poster from "./metaImg/poster.png";
-import Slider from "react-slick";
+import ScrollTriger from 'react-scroll-trigger'
+import img1 from './metaImg/book.png';
+import poster from './metaImg/poster.png'
 
-const SectionTwo = () => {
-  const [number, setNumber] = useState(false);
-  const sliderRef = useRef(null);
-  const array = [1, 2, 3, 4, 5, 7, 8, 9,]
-  const src =
-    "https://res.cloudinary.com/dbgyytugh/video/upload/v1665658464/recipes/MetateskTrailer_14.49.50_hmrro3.mp4";
+const Addsection4 = () => {
+  const [number, setNumber] = useState(false)
   
-    const [rightSliderClass, setrightSliderClass] = useState('');
-    const [lefttSliderClass, setlefttSliderClass] = useState('left-side');
 
-     
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    initialSlide: 0,
-    beforeChange: (current, next) => {
-      if(next > 0 && !rightSliderClass){
-        setrightSliderClass('right-side')
-      }
-      if(next === 0 && rightSliderClass){
-        setrightSliderClass('')
-        console.log(true)
-      }
-
-      if(array.length - 4 === next){
-        setlefttSliderClass('')
-      }else if(array.length - 4 != next){
-        setlefttSliderClass('left-side')
-      }
-
-    },
-  };
-  // console.log(slideIndex < [0, 1, 2, 3, 4, 5, 6].length - 1)
-  // console.log(slideIndex )
+  const src = "https://res.cloudinary.com/dbgyytugh/video/upload/v1665658464/recipes/MetateskTrailer_14.49.50_hmrro3.mp4"
   return (
-    <ScrollTriger
-      onEnter={() => setNumber(true)}
-      onExit={() => setNumber(false)}
-    >
-      <article className="section-two">
-        <div className="container">
+    <ScrollTriger onEnter={()=>  setNumber(true)} onExit={() => setNumber(false)} >
+      <article className='section-two'>
+        <div  className="container">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-md-12 ">
-                <div className="text-div">
-                  <p>Discover a world of endless learning possibilities</p>
-                  <p>
-                    We give kids 3–18 a place to build their own curriculum of
-                    interactive,
-                    <br /> one-of-a-kind classes.
-                  </p>
+              <div className="col-md-12">
+                <h3> shifting <span> EDUCATION </span>  from  </h3>
+                <div className="text-box text">
+                  <p> boring </p> <p> to </p> <p> exciting </p>
                 </div>
               </div>
-
-
-              <div className="col-md-12 slider-div">
-                <div className={rightSliderClass}>
-
-                </div>
-                <Slider {...settings}  >
-                {array.map(index => (
-                       <div key={index} className="slide-div">
-                       <div >
-                         <div className="top-div">
-                           <img
-                             src="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:140,width:240/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/uxkppTQSTROb91QPTjRT"
-                             alt=""
-                           />
-                         </div>
-                         <div className="bottom-div">
-                           <p> Lorem ipsum dolor sit. </p>
-                           <button>Davamı</button>
-                         </div>
-                       </div>
-                     </div>
-                  ))}
-                 
-                </Slider>
-                <div className={lefttSliderClass}>
-
+              <div className="col-md-6">
+                <div className="img-box">
+                  <img src="https://res.cloudinary.com/dbgyytugh/image/upload/v1666609835/book_jkiwjk.png" alt="metatesk" />
+                  <p>traditional learning</p>
                 </div>
               </div>
-
+              <div className="text-box-responsive text">
+                  <p> boring </p>
+                  <p>traditional learning</p>
+                  <p> to </p> 
+                  <p> exciting </p>
+                </div>
+              <div className="col-md-6">
+                <div className="video-box ">
+                <VideoPlayer
+                  src={src}
+                  // autoplay={true}
+                  controls={true}
+                  poster={poster}
+                  className="video"   
+                />
+                <p>immersive learning</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </article>
     </ScrollTriger>
-  );
-};
 
-export default SectionTwo;
+  )
+}
+
+export default Addsection4
